3330deb032c9401674a6cab7a7dc7a5a4186c6ac
AndreVictor60 <andre.victor60@free.fr>
2017-12-13 10:58:38 +0100

    fini

---
+++ user.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Partie 7 Exercice 2</title>
+  </head>
+  <body>
+    <p>Vous êtes sur la page User.php</p>
+    <!-- <?php
+      // if (isset($_POST['firstName']) AND isset($_POST['lastName']))
+      // {
+      //   echo 'Bonjour, ' . $_POST['firstName'] . ' ' . $_POST['lastName'];
+      // }
+     ?> -->
+  </body>
+</html>
