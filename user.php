19437923df14644a2098264d5adc7f3c9b4bdbd8
AndreVictor60 <andre.victor60@free.fr>
2018-01-03 10:57:22 +0100

    Commentaire

--- user.php
+++ user.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Partie 7 Exercice 2</title>
-  </head>
-  <body>
-    <p>Vous êtes sur la page User.php</p>
-    <!-- <?php
-      // if (isset($_POST['firstName']) AND isset($_POST['lastName']))
-      // {
-      //   echo 'Bonjour, ' . $_POST['firstName'] . ' ' . $_POST['lastName'];
-      // }
-     ?> -->
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Partie 7 Exercice 2</title>
+    </head>
+    <body>
+        <p>Vous êtes sur la page User.php</p>
+        <?php
+        // if (isset($_POST['firstName']) AND isset($_POST['lastName']))
+        // {
+        //   echo 'Bonjour, ' . htmlspecialchars($_POST['firstName']) . ' ' . htmlspecialchars($_POST['lastName']);
+        // }
+        ?>
+    </body>
 </html>
